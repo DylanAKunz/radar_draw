f498dde9a4e790ce46f5a5bf5bdcadc5c15b50d0
DylanAKunz <Dylan.a.Kunz@gmail.com>
2018-12-07 08:34:53 -0800

    Radar line draw
    Draws a radar line, has it rotate around the circle.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 #include <iostream>
 #include <cmath>
 
+const int BACKGROUND [3] = {0, 15, 0};
+const QColor RADARPLATECOLOR(40, 40, 50);
+
 int randNum(int min, int max)  //Generate a random number.
 {
     unsigned int N = (max - min <= RAND_MAX)
@@ -19,16 +22,14 @@ int randNum(int min, int max)  //Generate a random number.
 }
 
 //Youve implemeted the algorithim to get the end point, just need to draw the line from start to finish
-QImage radarLine(QImage image, int width, int height, int count){
+QImage radarLine(QImage image, int width, int height, float count, int red, int green, int blue, float opacity = 0.8){
     int center[2] = {width / 2, height / 2};
-    double radian = (count * M_PI);
+    int yCount = 0;
     int radius = (width + height) / 4;
-    bool x;
-    int x0 = center[0], y0 = center[1], red = 255, green = 5, blue = 5;
-    QColor color = (Qt::green);
-    QColor color2 = (Qt::red);
+    QColor color(red * opacity, green * opacity + BACKGROUND[1], blue * opacity);
+    int x0 = center[0], y0 = center[1];
     int x1 = (radius * cos(count)) + 150, y1 = (radius * sin(count)) + 150;
-    image.setPixelColor(x1, y1, color2);
+    image.setPixelColor(x1, y1, color);
     if(x0 > x1){
         std::swap(x0, x1);
         std::swap(y0, y1);
@@ -45,14 +46,18 @@ QImage radarLine(QImage image, int width, int height, int count){
     float error = 0.0;
     int y = y0;
     for(int x = x0; x <= x1; x++){
-        image.setPixelColor(x, y, color2);
+        image.setPixelColor(x, y, color);
         error += fabs(deltaError);
 
-        while(error >= 0.5){
+        while(error >= 0.5 && x <= x1){
 
             y += ((deltay > 0) - (deltay < 0));
-            image.setPixelColor(x, y, color2);
+            image.setPixelColor(x, y, color);
             error -= 1;
+            yCount++;
+            if(yCount >= 150){
+                x = x1 + 1;
+            }
         }
     }
 
@@ -63,8 +68,8 @@ QImage radarLine(QImage image, int width, int height, int count){
 QImage drawCircle(QImage image, int width, int height){
     int center[2] = {width / 2, height / 2};
     int radius = (width + height) / 4;
-    int x = center[0], y = center[1] + radius, red = 255, green = 255, blue = 255;
-    QColor color = (red, green, blue);
+    int x = center[0], y = center[1] + radius;
+    QColor color(BACKGROUND[0], BACKGROUND[1], BACKGROUND[2]);
     for(y = -radius; y <= radius; y++){
         for(x = -radius; x<= radius; x++){
             if(x*x+y*y <= radius*radius){
@@ -75,31 +80,50 @@ QImage drawCircle(QImage image, int width, int height){
     return image;
 }
 
+void delay()
+{
+    QTime dieTime= QTime::currentTime().addMSecs(1);
+    while (QTime::currentTime() < dieTime)
+        QCoreApplication::processEvents(QEventLoop::AllEvents, 1);
+}
+
 int main(int argc, char *argv[])
 {
+    bool stop = false;
     srand(time(NULL));//Seed the random function
     QApplication a(argc, argv);
-    QImage pi;
-    QPainter p(&pi);
     MainWindow w;
     w.show();
-    int dimensionX = 300, dimensionY = 300, count = -780;
-    QColor color = (30, 30, 30);
+    int dimensionX = 300, dimensionY = 300;
+    float count = 0.0;
+    QColor backgroundColor(BACKGROUND[0], BACKGROUND[1], BACKGROUND[2]);
     QLabel *label = new QLabel(
         QApplication::translate("childwidget", ""), &w);
     QImage radarPlate;
     radarPlate = QImage(dimensionX, dimensionY, QImage::Format_RGB32);
-    radarPlate.fill(color);
+    radarPlate.fill(RADARPLATECOLOR);
     radarPlate = drawCircle(radarPlate, dimensionX, dimensionY);
     label->setPixmap(QPixmap::fromImage(radarPlate));
-    do{
-        radarPlate = radarLine(radarPlate, dimensionX, dimensionY, count);
-        count++;
-        label->setPixmap(QPixmap::fromImage(radarPlate));
-    }while(count < 780);
     label->resize(dimensionX, dimensionY);
     label->move(100, 100);
-    label->show();
+    int green [3] = {1, 255, 10};
+    QColor color2 = (Qt::red);
+    do{
+        radarPlate = radarLine(radarPlate, dimensionX, dimensionY, count + 0.03, green[0], green[1], green[2], 1.0);
+        radarPlate = radarLine(radarPlate, dimensionX, dimensionY, count - 0.030, green[0], green[1], green[2], 0.8);
+        radarPlate = radarLine(radarPlate, dimensionX, dimensionY, count - 0.045, green[0], green[1], green[2], 0.7);
+        radarPlate = radarLine(radarPlate, dimensionX, dimensionY, count - 0.06, green[0], green[1], green[2], 0.6);
+        radarPlate = radarLine(radarPlate, dimensionX, dimensionY, count - 0.075, green[0], green[1], green[2], 0.5);
+        radarPlate = radarLine(radarPlate, dimensionX, dimensionY, count - 0.09, green[0], green[1], green[2], 0.4);
+        radarPlate = radarLine(radarPlate, dimensionX, dimensionY, count - 0.0105, green[0], green[1], green[2], 0.3);
+        radarPlate = radarLine(radarPlate, dimensionX, dimensionY, count - 0.120, green[0], green[1], green[2], 0.2);
+        radarPlate = radarLine(radarPlate, dimensionX, dimensionY, count - 0.135, green[0], green[1], green[2], 0.1);
+        radarPlate = radarLine(radarPlate, dimensionX, dimensionY, count - 0.150, green[0], green[1], green[2], 0);
+        count += 0.005;
+        label->setPixmap(QPixmap::fromImage(radarPlate));
+        label->show();
+        delay();
+    }while(stop != true);
 
     return a.exec();
 }
